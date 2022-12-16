f621a5a1a1dfab5679f84f42db6f3a7239962a4c
Jose Castro <JoseAneurys@me.com>
2022-12-16 13:29:21 -0500

    Third commit, JS complete. On to HTML & CSS

--- index.js
+++ index.js
@@ -121,30 +121,26 @@
 
 //create spaceship class
 class Spaceship {
-    constructor (name, hull, firepower, accuracy, battling, defeated) {
+    constructor (name, hull, firepower, accuracy) {
         this.name = name;
         this.hull = hull;
         this.firepower = firepower;
         this.accuracy = accuracy;
-        this.battling = true;
-        this.defeated = false;
     }
 
     //within class, create attack enemy method
-    shotsFired(enemy) {
+    shotsFired(opponent) {
+        let currentHull = opponent.hull;
         //spaceships firepower along with probability of successful hit...
-        let firepowerImpact = this.firepower + this.accuracy;
-        //...will impact opponent's hull
-        enemy.hull -= firepowerImpact;
+        if (Math.random() < this.accuracy) {
+            //...will impact opponent's hull
+            currentHull = opponent.hull -= this.firepower;
+            console.log(`SUCCESSFUL HIT! from ${this.name}`);
+            console.log(`Firepower = ${this.firepower}, ${opponent.name} Current Hull = ${currentHull}`);
+        } else {
+            console.log(`MISSED HIT! from ${this.name}`);
+        }
     }
-
-    //within class, create enemy attack method
-    // iveBeenHit(me) {
-    //     //enemy ship's firepower along with probability of successful hit...
-    //     let firepowerImpact = this.firepower + this.accuracy;
-    //     //...will impact opponent's hull
-    //     me.hull = (me.hull - firepowerImpact);
-    // }
 };
 
 //create enemy Fleet class with an empty array for ships to be pushed into
@@ -155,8 +151,12 @@ class Fleet {
 
     //within class, create method for pushing enemy fleet into ships array
     addShip(name) {
+        let hull = Math.floor(Math.random() * (7-3)) + 3;
+        let firepower = Math.floor(Math.random() * (5-2)) + 2
+        let accuracy = (Math.random() * .81-.6 + .6).toFixed(2)
+
         //enemy ships' hull, firepower, & accuracy must be generated randomly
-        this.enemies.push(new Spaceship(name, Math.floor(Math.random() * (7-3)) + 3, Math.floor(Math.random() * (5-2)) + 2, (Math.random() * .81-.6 + .6).toFixed(2)));
+        this.enemies.push(new Spaceship(name, hull, firepower, accuracy));
     }
 };
 
@@ -182,26 +182,23 @@ const battle =() => {
 
     //ussHelloWorld attacks enemy ships while hull is greater than 0
     for (i=0; i < enemies.length; i++) {
+        console.log(`${ussHelloWorld.name} VS. ${enemies[i].name}, Hull: ${enemies[i].hull}`);
+        console.log("");
         //while looping, if my ship ever reaches 0 game is over
         if (ussHelloWorld.hull <= 0) {
-            ussHelloWorld.battling = false;
-            ussHelloWorld.defeated = true;
             console.log(`${ussHelloWorld.name}, you've been defeated.`)
+            console.log("");
             break;
         }
         //keep iterating through list while my ship is > 0
         while (ussHelloWorld.hull > 0) {
-            console.log(`Your opponent: ${enemies[i].name}, Hull: ${enemies[i].hull}`);
-            console.log("");
             //attack!
             ussHelloWorld.shotsFired(enemies[i]);
             //check damage to enemy, if enemy ship < 0 then the game is over
             if (enemies[i].hull <= 0) {
-                enemies[i].battling = false;
-                enemies[i].defeated = true;
                 //print enemy's health & declare my win
-                console.log(`Enemy Hull: ${enemies[i].hull}. You've defeated the enemy!`)
-                console.log(`Your current hull: ${ussHelloWorld.hull}`);
+                console.log(`You've defeated the enemy!`);
+                console.log(`ussHelloWorld current hull: ${ussHelloWorld.hull}`);
                 console.log("");
                 // let response = window.prompt(`You've won! Current Hull:${currentHull} Would you like to continue?`)
                 // console.log(response);
@@ -210,12 +207,10 @@ const battle =() => {
                 break;
             //else, battle continues and enemy gets to attack my ship
             } else {
-                console.log(`ENEMY HIT! Enemy currently:${enemies[i].hull}`);
-                console.log("");
                 //enemy attacks!
                 enemies[i].shotsFired(ussHelloWorld);
-                //print damage done to me
-                console.log(`YOU'VE BEEN HIT! Current health:${ussHelloWorld.hull}`);
+                console.log("");
+                console.log(`Match continues... ${enemies[i].name}, Hull: ${enemies[i].hull}`);
                 console.log("");
             }
         }
