aaea808530897afe41184e79c9ec30388e84d7b8
Jose Castro <JoseAneurys@me.com>
2023-01-06 21:00:01 -0500

    Enemy defeated img updated, battle start onclick

--- index.js
+++ index.js
@@ -3,120 +3,32 @@
 // myShip.addEventListener(`click`, () => {
 //     let shipHullElement = document.getElementById(`my-ship-hull`);
 //     //we pretend the enemy ship attacks
-//     let currentHull = +shipHullElement.innerText;
-//     console.log(typeof +currentHull);
+//     let currentHullHTML = +shipHullElement.innerText;
+//     console.log(typeof +currentHullHTML);
 //     //enemy ship hit for 3 damage
 
 //     let enemyDamage = 3;
 
-//     let finalHull = currentHull - enemyDamage;
+//     let finalHull = currentHullHTML - enemyDamage;
 
 //     shipHullElement.innerText = finalHull
 // })
 
-// let alienShip1 = document.getElementById(`alien-Ship1`);
+// let alienShip1 = document.getElementById(`alien-ship1`);
 
 // alienShip1.addEventListener(`click`, () => {
 //     let alienship1HullElement = document.getElementById(`alien-ship1-hull`);
 //     //we pretend the enemy ship attacks
-//     let currentHull = +alienship1HullElement.innerText;
-//     console.log(typeof +currentHull);
+//     let currentHullHTML = +alienship1HullElement.innerText;
+//     console.log(typeof +currentHullHTML);
 //     //enemy ship hit for 3 damage
 
 //     let enemyDamage = 3;
 
-//     let finalHull = currentHull - enemyDamage;
+//     let finalHull = currentHullHTML - enemyDamage;
 
 //     alienship1HullElement.innerText = finalHull
 // })
-
-// let alienShip2 = document.getElementById(`alien-Ship2`);
-
-// alienShip2.addEventListener(`click`, () => {
-//     let alienship3HullElement = document.getElementById(`alien-ship2-hull`);
-//     //we pretend the enemy ship attacks
-//     let currentHull = +alienship2HullElement.innerText;
-//     console.log(typeof +currentHull);
-//     //enemy ship hit for 3 damage
-
-//     let enemyDamage = 3;
-
-//     let finalHull = currentHull - enemyDamage;
-
-//     alienship2HullElement.innerText = finalHull
-// })
-
-// let alienShip3 = document.getElementById(`alien-Ship3`);
-
-// alienShip3.addEventListener(`click`, () => {
-//     let alienship3HullElement = document.getElementById(`alien-ship3-hull`);
-//     //we pretend the enemy ship attacks
-//     let currentHull = +alienship3HullElement.innerText;
-//     console.log(typeof +currentHull);
-//     //enemy ship hit for 3 damage
-
-//     let enemyDamage = 3;
-
-//     let finalHull = currentHull - enemyDamage;
-
-//     alienship3HullElement.innerText = finalHull
-// })
-
-// let alienShip4 = document.getElementById(`alien-Ship4`);
-
-// alienShip4.addEventListener(`click`, () => {
-//     let alienship4HullElement = document.getElementById(`alien-ship4-hull`);
-//     //we pretend the enemy ship attacks
-//     let currentHull = +alienship4HullElement.innerText;
-//     console.log(typeof +currentHull);
-//     //enemy ship hit for 3 damage
-
-//     let enemyDamage = 3;
-
-//     let finalHull = currentHull - enemyDamage;
-
-//     alienship4HullElement.innerText = finalHull
-// })
-
-// let alienShip5 = document.getElementById(`alien-Ship5`);
-
-// alienShip5.addEventListener(`click`, () => {
-//     let alienship5HullElement = document.getElementById(`alien-ship5-hull`);
-//     //we pretend the enemy ship attacks
-//     let currentHull = +alienship5HullElement.innerText;
-//     console.log(typeof +currentHull);
-//     //enemy ship hit for 3 damage
-
-//     let enemyDamage = 3;
-
-//     let finalHull = currentHull - enemyDamage;
-
-//     alienship5HullElement.innerText = finalHull
-// })
-
-// let alienShip6 = document.getElementById(`alien-Ship6`);
-
-// alienShip6.addEventListener(`click`, () => {
-//     let alienship6HullElement = document.getElementById(`alien-ship6-hull`);
-//     //we pretend the enemy ship attacks
-//     let currentHull = +alienship6HullElement.innerText;
-//     console.log(typeof +currentHull);
-//     //enemy ship hit for 3 damage
-
-//     let enemyDamage = 3;
-
-//     let finalHull = currentHull - enemyDamage;
-
-//     alienship6HullElement.innerText = finalHull
-// })
-
-
-
-
-
-
-
-
 
 
 //create spaceship class
@@ -196,6 +108,8 @@ const battle =() => {
             ussHelloWorld.shotsFired(enemies[i]);
             //check damage to enemy, if enemy ship < 0 then the game is over
             if (enemies[i].hull <= 0) {
+                //image reflects enemy ship defeated
+                document.getElementById("alien-ship").src="./images/enemy_ship_dead.png";
                 //print enemy's health & declare my win
                 console.log(`You've defeated the enemy!`);
                 console.log(`ussHelloWorld current hull: ${ussHelloWorld.hull}`);
@@ -216,5 +130,3 @@ const battle =() => {
         }
     }
 };
-
-battle();
